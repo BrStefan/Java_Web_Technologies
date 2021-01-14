6e925557fe83c6544ca8bfc23dd0d1097af149cd
BrStefan <brasoveanu.stefan@gmail.com>
2021-01-15 00:24:50 +0200

    Swagger Added

--- BrStefan/src/main/java/com/BrStefan/BrStefan/RestaurantBookingPlatformApplication.java
+++ BrStefan/src/main/java/com/BrStefan/BrStefan/RestaurantBookingPlatformApplication.java
@@ -2,8 +2,10 @@ package com.BrStefan.BrStefan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class RestaurantBookingPlatformApplication {
 
 	public static void main(String[] args) {
